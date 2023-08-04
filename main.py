ab081933d51fcb1eb89edfe705a11770f537a68e
tiksharsh <tiksharsh@icloud.com>
2023-08-04 18:52:19 +0530

    added changes for logging mechanism

---
+++ main.py
@@ -0,0 +1,4 @@
+# from text_summarization_nlp.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from text_summarization_nlp.logging import logger
+
+logger.info("Welcome to nlp project")
